2f7d4ae5959260a751cf4cd52ffe7bf7c3ce227c
MauroGutMB <mgmbarros2006@gmail.com>
2026-09-18 22:09:05 -0300

    style(pendencias): corrige formatacao do resolver-todas.dto

--- backend/src/pendencias/dto/resolver-todas.dto.ts
+++ backend/src/pendencias/dto/resolver-todas.dto.ts
@@ -1,6 +1,9 @@
 import { ApiProperty } from '@nestjs/swagger';
 
 export class ResolverTodasDto {
-  @ApiProperty({ description: 'Quantas pendências desse aluno foram marcadas como resolvidas' })
+  @ApiProperty({
+    description:
+      'Quantas pendências desse aluno foram marcadas como resolvidas',
+  })
   resolvidas: number;
 }
